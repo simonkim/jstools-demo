15bde8bb733dd20cd1d0759c2429152b14ea7e7e
Simon Kim <spamhere@kaoma.net>
2016-07-11 15:14:19 +0900

    npm run release minizes output while npm run build generates simple bundle

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,9 +1,23 @@
+var webpack = require('webpack');
+
+/* RELEASE | DEV */
+var RELEASE = (process.env.npm_lifecycle_event === 'release');
+var plugins = RELEASE ? 
+                [new webpack.optimize.UglifyJsPlugin({
+                    compress: {
+                        warnings: false
+                    }
+                })] : 
+                [];
+var outputfilename = RELEASE ?  "hlsm3u8.min.js" : "hlsm3u8.js";
+
 module.exports = {
     context: __dirname,
     entry: "./index",
     output: {
         path: __dirname + "/public/dist",
-        filename: "hlsm3u8.js",
+        filename: outputfilename,
         library: "Hlsm3u8" 
-    }
+    },
+    plugins: plugins
 }
